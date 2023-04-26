0cf71d4948d96f9ffa98bff5564e77bb022223da
stevensc <stevens.chrisj@gmail.com>
2023-04-26 13:47:14 -0500

    working REST API

--- api/src/main/java/com/example/api/ApiApplication.java
+++ api/src/main/java/com/example/api/ApiApplication.java
@@ -2,10 +2,20 @@ package com.example.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class ApiApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
     @GetMapping("/api")
     public Map<String, Object> getApiResponse() {
         Map<String, Object> response = new HashMap<>();
