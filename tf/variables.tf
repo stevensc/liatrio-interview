376be5bd159d3d38f1765981ab90758e29bf57bf
stevensc <stevens.chrisj@gmail.com>
2023-04-27 11:46:13 -0500

    refactored providers

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,24 +1,24 @@
 variable "gcp_project_id" {
   type = string
   description = "Google Cloud project ID"
-  default = ""
+  default = null
 }
 
 variable "gcp_region" {
   type = string
   description = "Google Cloud region"
-  default = ""
+  default = null
 }
 
 variable "gcp_zone" {
   type = string
   description = "Google Cloud zone"
-  default = ""
+  default = null
 }
 
 variable "gcp_credentials" {
   type = string
   sensitive = true
   description = "Google Cloud service account credentials"
-  default = ""
+  default = null
 }
