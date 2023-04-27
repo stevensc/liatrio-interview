731fa5152e3162d80b6ca5a3ac0271faca3a2d3d
stevensc <stevens.chrisj@gmail.com>
2023-04-27 12:30:22 -0500

    prevent api destruction since this is for a demo

--- tf/main.tf
+++ tf/main.tf
@@ -1,11 +1,13 @@
 resource "google_project_service" "cloudresourcemanager" {
   service = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
 }
 
 resource "google_project_service" "artifactregistry" {
+  depends_on = [google_project_service.cloudresourcemanager]
   service = "artifactregistry.googleapis.com"
+  disable_on_destroy = false
 }
-
 
 
 resource "google_artifact_registry_repository" "docker-repo" {
