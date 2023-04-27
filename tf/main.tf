cb41592d67d0e43c222f5201306f27cba8561c87
stevensc <stevens.chrisj@gmail.com>
2023-04-27 12:14:05 -0500

    added cloudresourcemanager.googleapis.com

--- tf/main.tf
+++ tf/main.tf
@@ -1,6 +1,12 @@
+resource "google_project_service" "cloudresourcemanager" {
+  service = "cloudresourcemanager.googleapis.com"
+}
+
 resource "google_project_service" "artifactregistry" {
   service = "artifactregistry.googleapis.com"
 }
+
+
 
 resource "google_artifact_registry_repository" "docker-repo" {
   depends_on = [google_project_service.artifactregistry]
