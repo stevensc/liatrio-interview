4b5df78ecf2c25f8cbecf90251c42673cbb02484
stevensc <stevens.chrisj@gmail.com>
2023-04-27 11:57:53 -0500

    enabled artifactregistryapi

--- tf/main.tf
+++ tf/main.tf
@@ -1,5 +1,8 @@
+resource "google_project_service" "artifactregistry" {
+  service = "artifactregistry.googleapis.com"
+}
+
 resource "google_artifact_registry_repository" "docker-repo" {
-  project       = var.gcp_project_id
   location      = var.gcp_region
   repository_id = "docker-images"
   description   = "Repository for images running on k8s"
