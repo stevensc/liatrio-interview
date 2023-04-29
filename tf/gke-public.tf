48ccbc1c69f1eb7d79a7435007dc1af74aa3c77a
stevensc <stevens.chrisj@gmail.com>
2023-04-29 10:41:07 -0500

    duplicating locals to network

---
+++ tf/gke-public.tf
@@ -0,0 +1,14 @@
+module "gke" {
+  source                     = "terraform-google-modules/kubernetes-engine/google//modules/beta-autopilot-public-cluster"
+  project_id                 = var.gcp_project_id
+  name                       = "liatrio-demo-cluster"
+  region                     = "us-central1"
+  zones                      = ["us-central1-a", "us-central1-b", "us-central1-f"]
+  network                    = "vpc-01"
+  subnetwork                 = "us-central1-01"
+  ip_range_pods              = "us-central1-01-gke-01-pods"
+  ip_range_services          = "us-central1-01-gke-01-services"
+  horizontal_pod_autoscaling = false
+  filestore_csi_driver       = false
+
+}
