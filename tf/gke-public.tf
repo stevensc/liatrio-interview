68dbd5baa61750afc21ac8a7f61cefe0881b6b16
stevensc <stevens.chrisj@gmail.com>
2023-04-29 11:00:17 -0500

    enabled horizontal pod autoscaling

--- tf/gke-public.tf
+++ tf/gke-public.tf
@@ -19,7 +19,7 @@ module "gke" {
   subnetwork                 = local.subnet_names[index(module.gcp-network.subnets_names, local.subnet_name)]
   ip_range_pods              = local.pods_range_name
   ip_range_services          = local.svc_range_name
-  horizontal_pod_autoscaling = false
+  horizontal_pod_autoscaling = true
   enable_vertical_pod_autoscaling = true  
 }
 
