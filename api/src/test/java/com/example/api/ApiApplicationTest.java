939e2d9e15b5426ae5a9a8a17572ee23b25a2c0e
stevensc <stevens.chrisj@gmail.com>
2023-05-01 14:18:06 -0500

    trying new plugin

--- api/src/test/java/com/example/api/ApiApplicationTest.java
+++ api/src/test/java/com/example/api/ApiApplicationTest.java
@@ -1,50 +1,48 @@
+package com.example.api;
+
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.Assert;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(ApiApplication.class)
-class ApiApplicationTest {
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApiApplicationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private ApiApplication apiApplication;
-
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    @BeforeEach
-    public void setup() {
-        Map<String, Object> response = Map.of("message", "Automate all the things!", "timestamp", 1529729125);
-        when(apiApplication.getApiResponse()).thenReturn(response);
-    }
-
     @Test
-    void testGetApiResponse() throws Exception {
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+    public void testGetApiResponse() throws Exception {
+        MvcResult result = mockMvc.perform(get("/api")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andReturn();
 
         String content = result.getResponse().getContentAsString();
         Map<String, Object> responseMap = objectMapper.readValue(content, new TypeReference<Map<String, Object>>(){});
-
-        assertTrue(responseMap.containsKey("message"));
-        assertTrue(responseMap.containsKey("timestamp"));
+        Assert.isTrue(responseMap.containsKey("trash"), "key not fun");
+//        assertThat(responseMap.("message").isEqualTo("Automate all the things!");
+        assert(responseMap.containsKey("timestamp"));
+        
     }
 }
